b8134a9fe18db16f03cf318ee577b7ffb275deab
Hrijul Srivastava <60177785+hrijul09@users.noreply.github.com>
2020-04-21 22:06:05 +0530

    Add files via upload
    java program

---
+++ command1.java
@@ -0,0 +1,22 @@
+class command1
+{
+ public static void main(String args[])
+ {
+  
+System.out.println("my name is hrijul");
+System.out.println("roll no 68");
+   int a,b;
+   a=Integer.parseInt(args[0]);
+   b=Integer.parseInt(args[1]);
+	     System.out.println("sum is "+(a+b));
+		     System.out.println("diff is "+ (a-b));
+			     System.out.println("product is "+(a*b));
+				     System.out.println("diivide "+(a/b));
+					 }
+					 }
+
+
+
+
+	 
+	 
